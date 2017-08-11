505ebf4a1b0d9d9b06e607748f40d31d22dae2ef
Matt Hesby <mhesby@summitps.org>
2017-08-11 20:44:58 +0000

    first commit

---
+++ int-2-checkpoints/example.js
@@ -0,0 +1,19 @@
+var sketch = function(processing) {with(processing) {size(400, 400);background(255);
+// °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Below This Line ø¤º°`°º¤ø,¸,ø¤°//
+
+
+
+strokeWeight(3);
+stroke(57, 0, 214);
+fill(0, 210, 247);
+
+draw = function() {
+    background(255, 255, 255);
+    ellipse(mouseY, mouseX, 30, 30);
+};    
+    
+
+
+
+// °º¤ø,¸¸,ø¤º°`°º¤ø Create Project Above This Line ø¤º°`°º¤ø,¸,ø¤°//   
+}};var p = new Processing(document.getElementById("output-canvas"), sketch);
